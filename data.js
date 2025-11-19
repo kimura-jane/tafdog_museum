563a61636d9b57d6e89f838c801b081fd6fb5e8a
木村 <lakokubunji@gmail.com>
2025-11-19 23:00:34 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+// data.js
+window.WORKS = [
+  { id: 1,  title: 'TAF DOG #01', image: 'taf_dog_01.png' },
+  { id: 2,  title: 'TAF DOG #02', image: 'taf_dog_02.png' },
+  { id: 3,  title: 'TAF DOG #03', image: 'taf_dog_03.png' },
+  { id: 4,  title: 'TAF DOG #04', image: 'taf_dog_04.png' },
+  { id: 5,  title: 'TAF DOG #05', image: 'taf_dog_05.png' },
+  { id: 6,  title: 'TAF DOG #06', image: 'taf_dog_06.png' },
+  { id: 7,  title: 'TAF DOG #07', image: 'taf_dog_07.png' },
+  { id: 8,  title: 'TAF DOG #08', image: 'taf_dog_08.png' },
+  { id: 9,  title: 'TAF DOG #09', image: 'taf_dog_09.png' },
+  { id: 10, title: 'TAF DOG #10', image: 'taf_dog_10.png' },
+  { id: 11, title: 'TAF DOG #11', image: 'taf_dog_11.png' },
+  { id: 12, title: 'TAF DOG #12', image: 'taf_dog_12.png' },
+  { id: 13, title: 'TAF DOG #13', image: 'taf_dog_13.png' },
+  { id: 14, title: 'TAF DOG #14', image: 'taf_dog_14.png' },
+  { id: 15, title: 'TAF DOG #15', image: 'taf_dog_15.png' },
+  { id: 16, title: 'TAF DOG #16', image: 'taf_dog_16.png' },
+  { id: 17, title: 'TAF DOG #17', image: 'taf_dog_17.png' },
+  { id: 18, title: 'TAF DOG #18', image: 'taf_dog_18.png' }
+];
